aa8056230245a5452f055c44faaff7da2341d83c
ajay <023ajay.bhandari@gmail.com>
2025-05-13 21:41:22 +0530

    better three sum approach

--- leetcode/3sum.py
+++ leetcode/3sum.py
@@ -6,7 +6,7 @@ class Solution:
     Brute force solution to find the result (Time limit exceeded for large set of data)
     """
     def three_sum_brute_force(self, nums: List[int]) -> List[List[int]]:
-        # nums.sort()
+        nums.sort()
 
         result = []
         for i in range(len(nums)):
@@ -29,10 +29,44 @@ class Solution:
         return result
 
 
-arr = [-2, 0, 1, 1, 2]
+arr = [-1,0,1,2,-1,-4]
+# arr = [0,1,1]
 
 
 # s = Solution()
 # print(s.three_sum_brute_force(nums=arr))
 
 
+def three_sum(nums: List[int]) -> List[List[int]]:
+    nums.sort()
+
+    result = []
+
+    for i in range(len(nums)-2):
+        if nums[i] > 0:
+            break
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
+        left = i+1
+        right = len(nums) - 1
+
+        while left < right:
+            curr_sum = nums[left] + nums[right] + nums[i]
+            if curr_sum < 0:
+                left += 1
+            elif curr_sum > 0:
+                right -= 1
+            else:
+                result.append([nums[left], nums[right], nums[i]])
+
+                while left < right and nums[left] == nums[left+1]:
+                    left += 1
+                while left < right and nums[right] == nums[right - 1]:
+                    right -= 1
+                left += 1
+                right -= 1
+
+    return result
+
+
+print(three_sum(nums=arr))
