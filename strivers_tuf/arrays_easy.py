b23d7956f16e312608b68b677813313eab5942b1
ajay <023ajay.bhandari@gmail.com>
2025-05-15 22:34:26 +0530

    added algo for removal of duplicates elements from the array

--- strivers_tuf/arrays_easy.py
+++ strivers_tuf/arrays_easy.py
@@ -12,7 +12,42 @@ def check_sorted_array(arr):
     return True
 
 
-print(check_sorted_array(arr))
+# print(check_sorted_array(arr))
 
 
+def remove_duplicate_from_array(arr):
+    d = {}
+    l = len(arr)
+    i = 0
+    while i < l:
+        if arr[i] in d:
+            d[arr[i]] += 1
+            if d[arr[i]] > 1:
+                arr.remove(arr[i])
+                l -= 1
+        else:
+            d[arr[i]] = 1
+        i += 1
+    return arr
 
+
+def remove_duplicate_from_array_optimized(arr):
+    seen = set()
+    result = []
+    for i in arr:
+        if i not in seen:
+            seen.add(i)
+            result.append(i)
+    return result
+
+
+def remove_duplicate_from_sorted_array(arr):
+    result = [arr[0]]
+    for i in range(1, len(arr)):
+        if arr[i] != arr[i-1]:
+            result.append(arr[i])
+    return result
+
+
+check = [1, 1, 2, 2]
+print(remove_duplicate_from_sorted_array(arr=check))
