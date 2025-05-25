cead1a2a7bcb5c651519b3e7a9ba22b4db11104d
ajay <023ajay.bhandari@gmail.com>
2025-05-25 15:20:52 +0530

    sort 0 1 and 2

--- strivers_tuf/arrays_medium.py
+++ strivers_tuf/arrays_medium.py
@@ -25,3 +25,25 @@ def two_sum(arr, k):
 
 check1 = [1, 1, 3, 4, 5, 7]
 # print(two_sum(arr=check1, k=9))
+
+
+def sort0_1_2(arr):
+    mid = 0
+    left = 0
+    right = len(arr) - 1
+
+    while mid <= right:
+        if arr[mid] == 0:
+            arr[left], arr[mid] = arr[mid], arr[left]
+            mid += 1
+            left += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:
+            arr[right], arr[mid] = arr[mid], arr[right]
+            right -= 1
+    return arr
+
+
+check2 = [0, 1, 2, 1, 0, 2, 0, 1]
+print(sort0_1_2(arr=check2))
