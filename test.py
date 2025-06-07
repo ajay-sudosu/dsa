ef297a34f2737f68eaecdd84be94cab1315c75c6
ajay <023ajay.bhandari@gmail.com>
2025-06-07 07:49:05 +0530

    arrange array with sign refactor to replace in original array

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 def arrange_array_element_by_sign_brute_force(arr):
     pos = [i for i in arr if i > 0]
     neg = [i for i in arr if i < 0]
-    result = []
     for i in range(len(pos)):
-        result.append(pos[i])
-        result.append(neg[i])
-    return result
+        arr[2 * i] = pos[i]
+        arr[2 * i + 1] = neg[i]
+    return arr
 
 
-check10 = [1, -1, 2, -3]
+check10 = [-2, 1, -1, 2, -3, 4, ]
 print(arrange_array_element_by_sign_brute_force(check10))
